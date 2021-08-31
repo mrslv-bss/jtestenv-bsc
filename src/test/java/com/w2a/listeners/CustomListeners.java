33240bf790873114f718d66866134358a352d784
mrslv-bss <bassmiroslav@gmail.com>
2021-08-31 15:41:36 +0300

    Implemented email notifications into CustomListeners (commented)

--- src/test/java/com/w2a/listeners/CustomListeners.java
+++ src/test/java/com/w2a/listeners/CustomListeners.java
@@ -1,7 +1,14 @@
 package com.w2a.listeners;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -10,10 +17,13 @@ import org.testng.SkipException;
 
 import com.relevantcodes.extentreports.LogStatus;
 import com.w2a.base.TestBase;
+import com.w2a.utilities.MonitoringMail;
+import com.w2a.utilities.TestConfig;
 import com.w2a.utilities.TestUtilities;
 
-public class CustomListeners extends TestBase implements ITestListener {
+public class CustomListeners extends TestBase implements ITestListener,ISuiteListener {
 
+//	public String messageBody; // Uncomment for using email notification
 	public void onFinish(ITestContext arg0) {
 		// TODO Auto-generated method stub
 		
@@ -70,6 +80,37 @@ public class CustomListeners extends TestBase implements ITestListener {
 		test.log(LogStatus.PASS, arg0.getName().toUpperCase()+" PASS");
 		rep.endTest(test);
 		rep.flush();
+	}
+
+	public void onStart(ISuite suite) {
+		
+// 		Uncomment for using email notification
+//
+//		MonitoringMail mail = new MonitoringMail();
+//
+//		try {
+//			messageBody = "http://" + InetAddress.getLocalHost().getHostAddress() + ":8080/job/jtestenv/Extent_20Report/";
+//		} catch (UnknownHostException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//		System.out.println(messageBody);
+//		
+//		try {
+//			mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, messageBody);
+//		} catch (AddressException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (MessagingException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+	}
+
+	public void onFinish(ISuite suite) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
